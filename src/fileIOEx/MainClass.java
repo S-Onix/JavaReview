cac0b2985ca15e5d7310d4973aacb6d135c2489b
yms <yms@DESKTOP-7T9O25Q>
2018-01-24 16:30:11 +0900

    copy Ex

--- src/fileIOEx/MainClass.java
+++ src/fileIOEx/MainClass.java
@@ -7,21 +7,43 @@ import java.io.OutputStream;
 
 public class MainClass {
 	public static void main(String[] args) {
+		
+		InputStream is = null;
+		OutputStream os = null;
+		
 		try {
-			InputStream is = new FileInputStream("C:\\codeRepo\\workspace\\JavaReview\\example.txt");
+			is = new FileInputStream("C:\\codeRepo\\workspace\\JavaReview\\example.txt");
+			os = new FileOutputStream("C:\\codeRepo\\workspace\\JavaReview\\example_copy.txt");
+			
+			byte [] bs = new byte[5];
+			
 			while(true) {
-				int i = is.read();
+				int i = is.read(bs);
 				System.out.println("data : " + i );
 				if(i == -1) break;
+				os.write(bs, 0, i);
 			}
 			
-			OutputStream os = new FileOutputStream("C:\\codeRepo\\workspace\\JavaReview\\example.txt");
-			String str = "hello world";
-			byte[] bs = str.getBytes();
-			os.write(bs);
+
+			
 			
 		} catch (Exception e) {
 			// TODO: handle exception
+		} finally {
+			try {
+				if(is != null)
+					is.close();
+			} catch (Exception e2) {
+				// TODO: handle exception
+			}
+			try {
+				if(os != null)
+					os.close();
+			} catch (Exception e2) {
+				// TODO: handle exception
+			}
+			
 		}
+		
 	}
 }
